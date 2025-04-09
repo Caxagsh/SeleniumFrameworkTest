6162890e37b57c54e502a0dd5a105a2f52c4a681
Alexander <caxagsh@gmail.com>
2025-04-09 20:30:00 +0300

    Last update 09.04.2025 ver1.02

--- src/test/java/tests/ErrorValidationsTest.java
+++ src/test/java/tests/ErrorValidationsTest.java
@@ -1,8 +1,5 @@
 package tests;
 
-import com.aventstack.extentreports.ExtentReports;
-import com.aventstack.extentreports.reporter.ExtentReporter;
-import com.aventstack.extentreports.reporter.ExtentSparkReporter;
 import com.pageObject.CartPage;
 import com.pageObject.ProductCatalog;
 import org.testng.Assert;
